e29d43e57e48395af628efa38257bafb11773b29
Tardis3 <bagrianskii.liu@phystech.edu>
2021-07-10 18:07:16 +0300

    Started 2nd week.

---
+++ 2ndLesson/cmake-build-debug/CMakeFiles/2ndLesson.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2ndLesson.exe"
+  "2ndLesson.exe.manifest"
+  "2ndLesson.pdb"
+  "CMakeFiles/2ndLesson.dir/main.cpp.obj"
+  "lib2ndLesson.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2ndLesson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 2ndLesson/main.cpp
+++ 2ndLesson/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <vector>
-
-using namespace std;
 
 int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
