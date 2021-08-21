09eb4b8493001231ad7a64091b4be06c135e6e2b
Tardis3 <bagrianskii.liu@phystech.edu>
2021-08-21 14:17:44 +0300

    .

--- 3rdLesson/main.cpp
+++ 3rdLesson/main.cpp
@@ -3,148 +3,24 @@
 #include <algorithm>
 #include <string>
 #include <map>
-#define first 0
-#define second 1
-
 using namespace std;
 
-string createStrOfElements (const vector<string>& v); // создает строку "(v[l-1], v[l-1], .. v[0])"
-void deleteRepeats (vector<string>& v); // убирает повторяющиеся(подряд) жлементы массива и удаляет последний элемент
 
-
-// Реализуйте класс для человека, поддерживающий историю изменений человеком своих фамилии и имени.
-class Person {
-public:
-    void ChangeFirstName(int year, const string& new_first_name) {
-
-        nameHystory[year][first] = new_first_name;
-        changeFirstNameYears.push_back(year);
-
-        // добавить факт изменения имени на first_name в год year
-    }
-    void ChangeLastName(int year, const string& new_last_name) {
-        nameHystory[year][second] = new_last_name;
-        changeSecondNameYears.push_back(year);
-;
-        // добавить факт изменения фамилии на last_name в год year
-    }
-
-
-
-    string GetFullName(int year) {
-        string first_name = GetNumberName(year, changeFirstNameYears, first);
-        string second_name = GetNumberName(year, changeSecondNameYears, second);
-
-        if (first_name.empty() && second_name.empty())
-            return "Incognito";
-        if (second_name.empty())
-            return first_name + " with unknown last name";
-        if (first_name.empty())
-            return second_name + " with unknown first name";
-        return first_name + ' ' + second_name;
-        // получить имя и фамилию по состоянию на конец года year
-    }
-
-    string GetFullNameWithHistory (int year)  // получить все имена и фамилии по состоянию на конец года year
-    {
-        string first_name = GetNumberName(year, changeFirstNameYears, first);
-        vector<string> first_names;
-        int i = 0;
-        while (i < changeFirstNameYears.size() && year >= changeFirstNameYears[i]) {
-            first_names.push_back(nameHystory[changeFirstNameYears[i]][first]);
-            i++;
-        }
-        deleteRepeats(first_names);
-
-        string second_name = GetNumberName(year, changeSecondNameYears, second);
-        vector<string> second_names;
-        i = 0;
-        while (i < changeSecondNameYears.size() && year >= changeSecondNameYears[i]) {
-            second_names.push_back(nameHystory[changeSecondNameYears[i]][second]);
-            i++;
-        }
-        deleteRepeats(second_names);
-
-        if (first_name.empty() && second_name.empty())
-            return "Incognito";
-        if (second_name.empty())
-            return first_name + ' ' + createStrOfElements(first_names) + "with unknown last name";
-        if (first_name.empty())
-            return second_name + ' ' + createStrOfElements(second_names) + "with unknown first name";
-        string result = first_name + ' ' + createStrOfElements(first_names) +
-                        second_name + ' ' + createStrOfElements(second_names);
-        result.resize(result.size() - 1);
-        return result;
-
-    }
-
-private:
-    map<int, string[2]> nameHystory;
-
-    vector<int> changeFirstNameYears;  // года, хранящие изменения имен.
-    vector<int> changeSecondNameYears; // года, хранящие изменения фамилий.
-
-    string GetNumberName(int year, vector<int>& changeNameY, int nameNumber){
-        if (changeNameY.empty())
-            return "";
-        sort(changeNameY.begin(), changeNameY.end());
-        if (changeNameY.empty() || changeNameY[0] > year)
-            return "";
-
-        int i = 0;
-        while ((i + 1) < changeNameY.size() && changeNameY[i + 1] <= year)  // в векторе с годами изменений имени
-            i++;                                                           // ищем ближайший последний к year год
-
-        return nameHystory[changeNameY[i]][nameNumber];
-    }
-
-    // приватные поля
+struct Incognizable
+{
+    int first;
+    int second;
 };
 
-int main(){
-    Person person;
 
-    person.ChangeFirstName(1900, "Eugene");
-    person.ChangeLastName(1900, "Sokolov");
-    person.ChangeLastName(1910, "Sokolov");
-    person.ChangeFirstName(1920, "Evgeny");
-    person.ChangeLastName(1930, "Sokolov");
-    cout << person.GetFullNameWithHistory(1940) << endl;
+int main() {
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {6};
+    Incognizable d = {0, 1};
+    a.first = 0;
+    vector<int> arr = {0, 1};
 
     return 0;
 
 }
-
-string createStrOfElements (const vector<string>& v)
-{
-
-    if (v.empty())
-        return "";
-
-    string result = "(";
-    for (int i = v.size() - 1; i > 0; --i) {
-        result += (v[i] + ", ");
-
-    }
-    result += (v[0] + ") ");
-    return result;
-}
-
-void deleteRepeats (vector<string>& v)
-{
-    if (v.size() == 1)
-        v.clear();
-    else if (v.size() >= 2)
-    {
-        vector<string> new_v;
-        new_v.push_back(v[0]);
-        for (int i = 1; i < v.size(); ++i) {
-            if (v[i] != new_v.back())
-                new_v.push_back(v[i]);
-        }
-        new_v.resize(new_v.size() - 1);
-        v = new_v;
-    }
-
-}
-
