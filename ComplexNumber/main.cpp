27ecb19b98241d1e352a62f6b27866471a39d795
levBagryansky <28lev11@gmail.com>
2021-09-27 13:38:46 +0300

    .

--- ComplexNumber/main.cpp
+++ ComplexNumber/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include "ComplexNum.h"
+#include "Tests/test1.cpp"
 
 int main() {
-    ComplexNum z1 = ComplexNum(5, 7);
-    ComplexNum z2;
-    std::cout << "Enter complex number (a + bi):";
-    z2.operator>>(std::cin);
-    z1.AbsSquare();
-    std::cout << "Your complex number equal ";
-    z2.operator<<(std::cout) << std::endl;
+    test1();
     return 0;
 }
