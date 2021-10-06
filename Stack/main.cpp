60f1e62dfbda1d9468018cc79046f7061c3ab063
levBagryansky <28lev11@gmail.com>
2021-10-07 00:31:11 +0300

    Добавил тесты для IsEmpty, ==

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -5,6 +5,9 @@
 int test();
 int correctPushAndGetSize();
 int correctPop();
+int correctTop();
+int correctIsEmpty();
+int correctEqual();
 
 int main() {
     test();
@@ -19,15 +22,30 @@ int test(){
         std::cout << "Push or GetSize works incorrect: test " << errCode << std::endl;
         errSum++;
     }
+
     if (errCode = correctPop()){
         std::cout << "Pop works incorrect: test " << errCode << std::endl;
+        errSum++;
+    }
+
+    if (errCode = correctTop()){
+        std::cout << "Top works incorrect: test " << errCode << std::endl;
+        errSum++;
+    }
+
+    if (errCode = correctIsEmpty()){
+        std::cout << "IsEmpty works incorrect:  test " << errCode << std::endl;
+        errSum++;
+    }
+
+    if (errCode = correctEqual()){
+        std::cout << "Operator == works incorrect:  test " << errCode << std::endl;
         errSum++;
     }
 
     if (!errSum){
         std::cout << "Nice!" << std::endl;
     }
-
     return errSum;
 }
 
@@ -86,5 +104,87 @@ int correctPop(){
     if (v1 != vExampleDouble && v1.size() != 9){
         return 2;
     }
+    return 0;
+}
+
+int correctTop(){
+    Stack<int> s;
+    for (int i = 0; i < 999; ++i) {
+        s.push(i * 2);
+    }
+    int lastNum = s.top();
+    if (lastNum != (2 * 998)){
+        return 1;
+    }
+
+    s.push(-99909);
+    if (s.top() != -99909){
+        return 2;
+    }
+
+    Stack<double> doubles;
+    for (int i = 0; i < 432; ++i) {
+        doubles.push(i / 2);
+    }
+    double lastDouble = doubles.top();
+    if (abs(lastDouble - 431/2) > 1e-8){
+        return 3;
+    }
+
+    return 0;
+}
+
+int correctIsEmpty(){
+    Stack<double>  doubles{5};
+   if(!doubles.isEmpty()){
+       return 1;;
+   }
+
+    doubles.push(4);
+    if (doubles.isEmpty()){
+        return 2;
+    }
+
+    doubles.pop();
+    if (!doubles.isEmpty())
+    {
+        return 3;
+    }
+
+    return 0;
+}
+
+int correctEqual(){
+    Stack<int> ints1;
+    Stack<int> ints2;
+    if (!(ints1 == ints2)){
+        return 1;
+    }
+
+    Stack<double> doubles1;
+    Stack<double> doubles2;
+    if (!(doubles1 == doubles2)){
+        return 2;
+    }
+
+    for (int i = 0; i < 900; ++i) {
+        ints1.push(i);
+    }
+    for (int i = 0; i < 100; ++i) {
+        ints1.pop();
+    }
+    for (int i = 0; i < 800; ++i) {
+        ints2.push(i);
+    }
+
+    if (!(ints1 == ints2)){
+        return 3;
+    }
+
+    ints1.push(9);
+    if (ints1 == ints2){
+        return 4;
+    }
+
     return 0;
 }
