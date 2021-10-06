f75b7ba2fab46cc65da5e9a5b8b42d11aa55ff4d
levBagryansky <28lev11@gmail.com>
2021-10-07 00:33:29 +0300

    Реализация операторов = копирования и перемещения, IsEmpty, оператора ==

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -24,6 +24,11 @@ int Stack<T>::getSize() const {
 }
 
 template<class T>
+bool Stack<T>::isEmpty() const{
+    return !size_;
+}
+
+template<class T>
 void Stack<T>::push(T value) {
     if (size_ == capacity_){
         T* newData = new T[2 * capacity_];
@@ -38,9 +43,61 @@ void Stack<T>::push(T value) {
 }
 
 template<class T>
-T Stack<T>::pop() {
+T Stack<T>::pop(){
+    if(size_ == 0){
+        exit(1);
+    }
     size_--;
     return data_[size_];
+}
+
+template<class T>
+T Stack<T>::top() const {
+    if (size_ == 0){
+        exit(2);
+    }
+
+    return data_[size_ - 1];
+}
+
+template<class T>
+Stack<T>& Stack<T>::operator=(const Stack<T> &other) {
+    if (this == &other){
+        return *this;
+    }
+
+    capacity_ = other.capacity_;
+    size_ = other.size_;
+
+    data_ = new T[capacity_];
+    std::copy(other.data_, other.data_ + size_, data_);
+    return *this;
+}
+
+template<class T>
+Stack<T>& Stack<T>::operator=(Stack<T>&& other) {
+    if (this == &other){
+        return *this;
+    }
+
+    capacity_ = other.capacity_;
+    size_ = other.size_;
+    data_ = other.data_;
+    other.data_ = nullptr;
+}
+
+template<class T>
+bool Stack<T>::operator==(const Stack<T> &other) {
+    if (size_ != other.size_){
+        return false;
+    }
+    for (int i = 0; i < size_; ++i) {
+        if (data_[i] != other.data_[i]){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 template<class T>
@@ -51,10 +108,4 @@ std::ostream& operator<<(std::ostream &os, Stack<T> &s){
     }
 
     return os;
-}
-
-template <class T>
-T minimum(const T& lhs, const T& rhs)
-{
-    return lhs < rhs ? lhs : rhs;
 }
