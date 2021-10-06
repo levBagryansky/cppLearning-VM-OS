60931ed3d636bb52238fd6671f9efdf94e392c85
levBagryansky <28lev11@gmail.com>
2021-10-07 00:32:28 +0300

    Обявил метод IsEmpty, operator==

--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -12,12 +12,14 @@ public:
     ~Stack();
 
     int getSize() const;
+    bool isEmpty() const;
     void push(T x);
     T pop();
     T top() const;
 
-    Stack &operator=(const Stack &other);
-    Stack &operator=(Stack &&other);
+    Stack& operator=(const Stack &other);
+    Stack& operator=(Stack &&other);
+    bool operator== (const Stack<T>& other);
 
     template<class U>
     friend std::ostream& operator<<(std::ostream &os, Stack<U> &s);
@@ -28,8 +30,5 @@ private:
     int capacity_;
 };
 
-template<class T>
-T minimum(const T &lhs, const T &rhs);
-
 #include "Stack.cpp"
-#endif
+#endif // STACK_HPP
