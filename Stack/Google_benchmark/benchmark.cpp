d9f74089738e5c6cf004af68653147fc9f971687
levBagryansky <28lev11@gmail.com>
2021-10-25 21:14:18 +0300

    Добавил бенчмарк

--- Stack/Google_benchmark/benchmark.cpp
+++ Stack/Google_benchmark/benchmark.cpp
@@ -1,27 +1,27 @@
 #include <benchmark/benchmark.h>
 #include "../Bool_Stack.h"
 
-void fillStack(int factor){
+void fillStack(long double factor){
     Stack<int> stack;
     for (int i = 0; i < 1e6; ++i) {
         stack.push(rand());
     }
 }
 
-
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state)
-        std::string empty_string;
+static void CustomArguments(benchmark::internal::Benchmark* b) {
+    for (int i = 150; i <= 250; ++i)
+        b->Arg({i});
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-    std::string x = "hello";
-    for (auto _ : state)
-        std::string copy(x);
+static void BM_fillStack(benchmark::State& state){
+    double factor = double(state.range(0)) / 100;
+    for (auto _ : state) {
+        Stack<int> stack;
+        for (int i = 0; i < 1e6; ++i) {
+            stack.pushWithFactor(rand(), factor);
+        }
+    }
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_fillStack) ->Apply(CustomArguments) -> Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
