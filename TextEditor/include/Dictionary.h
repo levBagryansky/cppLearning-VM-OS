605eef85af2006eef5b30e2d660f3b857d1e2083
levBagryansky <28lev11@gmail.com>
2022-04-05 18:11:51 +0300

    added GetSuitableDictionary method

--- TextEditor/include/Dictionary.h
+++ TextEditor/include/Dictionary.h
@@ -1,7 +1,7 @@
 #ifndef DICTIONARY_IMPL_H
 #define DICTIONARY_IMPL_H
 
-#include "HashTable.h"
+#include "HashTable-impl.h"
 #include <fstream>
 
 bool CorrectSymbol(char c);
@@ -12,16 +12,15 @@ class Dictionary : public HashTable<std::string>{
    public:
     Dictionary(int min_len, int max_len);
     explicit Dictionary(int len = 0);
-    void SetLen(int len);
-    void SetLen(int min_len, int max_len);
+    void SetLen(uint len);
+    void SetLen(uint min_len, uint max_len);
     void Update(const std::string& path);
-    const std::string& BestWord(const std::string& word);
+    const std::string& BestWord(const std::string& word) const;
+    void AddKey(const std::string& key);
 
    private:
     uint min_len_;
     uint max_len_;
-
-    void AddKey(const std::string& key);
 };
 
 #endif //DICTIONARY_IMPL_H
--- TextEditor/include/TextEditor.h
+++ TextEditor/include/TextEditor.h
@@ -14,15 +14,17 @@ class TextEditor{
     ~TextEditor();
 
     void Upload(const std::string& path);
-    void DumpStatistics();
-    bool HaveWord(const std::string& word);
-    void EditWord(std::string *p_str);
-    void EditVectorRange(std::vector<std::string> *p_vector, size_t start, size_t range_len);
-    void EditText(const std::string& wrong_text, const std::string& correct_text, int n_threads = 100);
+    void DumpStatistics() const;
+    bool HaveWord(const std::string& word) const;
+    void EditWord(std::string *p_str) const;
+    void EditVectorRange(std::vector<std::string> *p_vector, size_t start, size_t range_len) const;
+    void EditText(const std::string& wrong_text_path, const std::string& correct_text_path, int n_threads = 100) const;
 
    private:
     uint num_of_tables_;
     Dictionary* tables_;
+
+    size_t GetSuitableDictionary(size_t word_len) const;
 };
 
 #endif //TEXTEDITOR_H
